d0b67ea6870a918c0165ed9c15180185648f25b3
Thomas Leupp <tomleupp@gmail.com>
2016-01-23 08:56:24 -0800

    Added front-end routes

---
+++ client/app.js
@@ -0,0 +1,51 @@
+angular.module('budgie', [])
+  .config(function($stateProvider, $urlRouterProvider, $httpProvider){
+
+	$stateProvider
+		.state('main', {
+			url: '/',
+			templateUrl: './main/main.html',
+			controller: 'MainController as main'
+		})
+      	.state('main.expense', {
+       		url: '/expense',
+        	templateUrl: './main/expense/expense.html',
+        	controller: 'ExpenseController as expense'
+      	})
+      	.state('main.profile', {
+      		url: '/profile',
+      		templateUrl: './main/profile/profile.html',
+      		controller: 'ProfileController as profile'
+      	})
+        .state('main.history', {
+        	url: '/history',
+        	templateUrl: './main/history/history.html',
+        	controller: 'HistoryController as history'
+
+      	})
+		.state('main.learn', {
+			url: '/learn',
+			templateUrl: './main/learn/learn.html',
+			controller: 'LearnController as learn'
+		})
+		.state('main.stocks', {
+			url: '/stocks',
+			templateUrl: './main/stocks/stocks.html',
+			controller: 'StocksController as stocks'
+		})
+		.state('login', {
+			url: '/login',
+			templateUrl: './auth/login.html',
+			controller: 'LoginController as login'
+		})
+		.state('signup', {
+			url: '/signup',
+			templateUrl: './auth/signup.html',
+			controller: 'SignupController as signup'
+		});
+
+		$urlRouterProvider.otherwise('/');
+		
+	//  $httpProvider.interceptors.push('AttachTokens');   // commented out till we get Auth set up
+
+})
