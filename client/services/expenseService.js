51a02d07c6eb94c0ad538d452421de4978c5b6ba
kaushiksahoo2000 <kaushiksahoo2000@gmail.com>
2016-01-27 12:01:02 -0800

    [Feature] added front-end support on main page for incomes

--- client/services/expenseService.js
+++ client/services/expenseService.js
@@ -1,18 +1,23 @@
 angular.module('expense.service', [])
 .factory('ExpenseServices', function($http, $location, $window) {
 
- var submitNewExpense = function(expenseData) {
+ var submitNewExpense = function(expenseData, inputType) {
+   if(inputType === "income"){
+     expenseData.income_date = expenseData.spent_date;
+   }
+   console.log("this is the url, that is being used", ('/'+inputType+"s"));
    return $http({
      method: 'POST',
-     url: '/expenses',
+     url: '/'+inputType+"s",
      data: expenseData
    }).then(function(resp) {
     //  console.log("resp in submitNewExpense ", resp);
      return resp.data;
    }, function(error) {
-       console.error('Add Expense ERROR!!! ', error);
+       console.error('Submit Expense ERROR!!! ', error);
    });
  };
+
 
 
  var getExpensesForDays = function(days){
@@ -23,25 +28,41 @@ angular.module('expense.service', [])
     //  console.log("resp in getExpensesForDays ", resp);
      return resp.data;
    }, function(error) {
-     console.error('Add Expense ERROR!!! ', error);
+     console.error('Get Expense ERROR!!! ', error);
    });
  };
 
- var deleteExpense = function(id){
+ var deleteExpense = function(id, inputType){
+   if(inputType === "income"){
+     expenseData.income_date = expenseData.spent_date;
+   }
    return $http({
      method: 'DELETE',
-     url: '/expenses/' + id,
+     url: '/'+inputType+"s/"+id,
    }).then(function(resp) {
      console.log("resp in deleteExpense ", resp);
      return resp.data;
    }, function(error) {
-     console.error('Add Expense ERROR!!! ', error);
+     console.error('Delete Expense ERROR!!! ', error);
+   });
+ };
+
+ var getIncomesForDays = function(days){ //in refactor add to financeService
+   return $http({
+     method: 'GET',
+     url: '/incomes/' + days,
+   }).then(function(resp) {
+    //  console.log("resp in getExpensesForDays ", resp);
+     return resp.data;
+   }, function(error) {
+     console.error('Get Income ERROR!!! ', error);
    });
  };
 
  return {
    submitNewExpense: submitNewExpense,
    getExpensesForDays: getExpensesForDays,
+   getIncomesForDays: getIncomesForDays,
    deleteExpense: deleteExpense,
  };
 });
