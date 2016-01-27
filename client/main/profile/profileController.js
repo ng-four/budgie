ea4ca29a468e9803549832b81b510f002f33e558
Thomas Leupp <tomleupp@gmail.com>
2016-01-26 17:35:15 -0800

    added skeleton for goals on profile view

--- client/main/profile/profileController.js
+++ client/main/profile/profileController.js
@@ -2,16 +2,42 @@ angular.module('profile.controller', [])
 .controller('ProfileController', function(ProfileServices, AuthServices, $timeout, $window, $location){
 	var profile = this;
 
+	var fakeGoals = [{
+		name: "Rocket Car",
+		price: 20000,
+		target_date: null,
+		progress: 872,       // will need allocation functions to adjust
+		category: null
+		},
+		{
+		name: "Solid Gold House",
+		price: 45000,
+		target_date: null,
+		progress: 247,
+		category: null
+		},
+		{
+		name: "Mona Lisa",
+		price: 120000,
+		target_date: null,
+		progress: 89,
+		category: null
+		}];
+
 	profile.loadProfile = function() {
 		console.log("profile.loadProfile called ");
 		ProfileServices.getProfileData()
 			.then(function(resp){
 				profile.limitClicked = false;
 				profile.savingsClicked = false;
+				profile.allocateClicked = false;
+				profile.newGoalClicked = false;
+				profile.total_savings = resp.total_savings;
 				profile.email = resp.email;
 				profile.full_name = resp.full_name;			
 				profile.monthly_limit = resp.monthly_limit;
 				profile.savings_goal = resp.savings_goal;
+				profile.goals = resp.goals || fakeGoals;
 			}, function(error){
 				throw error;
 			});
@@ -46,9 +72,42 @@ angular.module('profile.controller', [])
 			});
 	};
 
+	profile.toggleAllocate = function(){
+		profile.allocateClicked = true;
+	}
+
+	profile.allocateSavings = function() {
+
+		//TODO
+
+	}
+
 	profile.logOut = function() {
 		AuthServices.logOut();			
 	};
+
+	profile.toggleNewGoal = function(){
+		if(!profile.newGoalClicked){
+			profile.newGoalClicked = true;
+		} else {
+			profile.newGoalClicked = false;
+		}
+	}
+
+	profile.submitNewGoal = function(goal){
+		profile.toggleNewGoal();
+		var newGoal = {
+			name: goal.name,
+			price: goal.price,
+			target_date: goal.target_date,
+			progress: 0
+		}
+
+											// placeholder till we get goal functionality on backend
+
+		fakeGoals.push(newGoal);
+		profile.loadProfile();
+	}
 
 	/* TODO:
 
