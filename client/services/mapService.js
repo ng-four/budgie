9ff544fd1ecb0482933d2ba7bbe183918be9b749
Thomas Leupp <tomleupp@gmail.com>
2016-01-29 10:03:35 -0800

    [feature] more work on google maps integration

---
+++ client/services/mapService.js
@@ -0,0 +1,17 @@
+
+
+
+angular.module('map.service', [])
+  .factory('MapServices', function($http, $location, $window) {
+
+
+// make an infoWindow in regular google maps api
+
+
+  return {
+
+
+  }
+
+
+});
