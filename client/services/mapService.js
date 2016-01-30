7b593f9521f03d96e8bcba248dac9f7d146d7bb9
Thomas Leupp <tomleupp@gmail.com>
2016-01-30 11:12:13 -0800

    [feature] google places autocomplete in place, refactoring map view

--- client/services/mapService.js
+++ client/services/mapService.js
@@ -1,17 +1,80 @@
-
-
-
 angular.module('map.service', [])
-  .factory('MapServices', function($http, $location, $window) {
+  .factory('MapServices', function($http, $location, $window, $q) {
 
+  	var makeMap = function(mapCanvas) {
+  		var map = new google.maps.Map(mapCanvas, {
+    		zoom: 15,
+    		center: {lat: 34.0210487, lng: -118.4922354}
+  		});
+  		return map;
+  	}
 
-// make an infoWindow in regular google maps api
+  	var getGeoCode = function(address){
+
+  		var deferred = $q.defer();
+  		var geocoder = new google.maps.Geocoder();
+
+    	var geocodeOptions = {
+      		address: address
+    	};
+
+    	geocoder.geocode(geocodeOptions, function(results, status) {
+      		if ( status !== google.maps.GeocoderStatus.OK ) {
+      			console.log("Geocoder failed!!! ", status);
+        		// deffered.reject('Geocoder failed due to: ' + status);
+      		}
+      		deferred.resolve(results[0].geometry.location);	
+    	});
+
+    	return deferred.promise;  // need to call lat() and lng()
+
+  	}
+
+  	var renderMarker = function(transaction, map, bounds){
+  		var date = transaction.spent_date || "";
+
+  		var contentString = '<div id="content">'+
+      		'<div id="bodyContent">'+
+      		'<p style="color: black">' + transaction.name + '</p>'+
+       		'<p style="color: black">' + transaction.category + '</p>'+
+      		'<p style="color: black">$' + transaction.amount + '</p>'+
+      		'<p style="color: black">' + date + '</p>'+
+      		'</div>'+
+      		'</div>';
+
+  		var infowindow = new google.maps.InfoWindow({
+    		content: contentString
+  		});
+
+  		var marker = new google.maps.Marker({
+    		position: transaction.latlng,  // how are we getting this lat/lng info???
+    		map: map,
+    		title: transaction.name
+  		});
+
+  		marker.addListener('mouseover', function() {
+    		infowindow.open(map, marker);
+  		});
+
+  		marker.addListener('mouseout', function() {
+    		infowindow.close(map, marker);
+  		});
+
+  		bounds.extend(transaction.latlng);
+    
+
+  	}
+
+  	var setBounds = function(map, bounds){
+  		map.fitBounds(bounds);
+  	}
 
 
   return {
-
+  	makeMap: makeMap,
+  	getGeoCode: getGeoCode,
+  	renderMarker: renderMarker,
+  	setBounds: setBounds
 
   }
-
-
 });
