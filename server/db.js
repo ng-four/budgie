455c78a904a3d9caa88e6ca79324f1743c21d6f3
kaushiksahoo2000 <kaushiksahoo2000@gmail.com>
2016-02-01 15:50:13 -0800

    [Testing] heroku database

--- server/db.js
+++ server/db.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 
 
 if (process.env.CLEARDB_DATABASE_URL) {
-  var db = mysql.createConnection(process.env.CLEARDB_DATABASE_URL + "&multipleStatements=true");
+  var db = mysql.createConnection(process.env.CLEARDB_DATABASE_URL );
+  // + "&multipleStatements=true"
 } else {
 // Connect to local MySql database
   var db = mysql.createPool({
@@ -18,19 +19,18 @@ if (process.env.CLEARDB_DATABASE_URL) {
 
 // db.on('error', function(){console.log("ERROR LOG FOR DAYS, ERROR ERROR ERROR");});
 // Read MySql schema and create tables if necessary
-fs.readFile(__dirname + '/setup.sql', 'utf-8', function(err, data){
-  if (err) {
-    console.error(err);
-  } else {
-    db.query(data, function(err, results, fields){
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('database successfully created');
-        // db.destroy();
-      }
-    });
-  }
-});
+// fs.readFile(__dirname + '/setup.sql', 'utf-8', function(err, data){
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     db.query(data, function(err, results, fields){
+//       if (err) {
+//         console.error(err);
+//       } else {
+//         console.log('database successfully created');
+//       }
+//     });
+//   }
+// });
 
 module.exports = db;
